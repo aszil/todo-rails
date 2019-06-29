a8bffa717d65b7502b216a361e643893ce2676b2
Arai Shinya <>
2019-06-29 14:42:55 +0900

    一応基本的なcrudはできた

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  post '/tasks', to: 'tasks#store'
+  get '/new', to: 'tasks#new'
+  get '/show/:id', to: 'tasks#show', as: 'show'
+  get '/edit/:id', to: 'tasks#edit', as: 'edit'
+  put '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy', as: 'destroy'
 end
